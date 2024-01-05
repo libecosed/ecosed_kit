764b3f8c48c6a88f363d531b3c0c0966e682d7e9
王泳淇 <815099246@qq.com>
2024-01-05 23:24:55 +0800

    commit

--- android/src/main/kotlin/io/ecosed/kit/EcosedKitPlugin.kt
+++ android/src/main/kotlin/io/ecosed/kit/EcosedKitPlugin.kt
@@ -1,30 +1,202 @@
 package io.ecosed.kit
 
+import android.Manifest
 import android.app.Activity
+import android.app.Notification
+import android.app.Service
+import android.app.UiModeManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
+import android.content.ServiceConnection
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import com.blankj.utilcode.util.AppUtils
+import com.blankj.utilcode.util.PermissionUtils
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import rikka.shizuku.Shizuku
+import kotlin.system.exitProcess
 
-class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.MethodCallHandler,
-    ActivityAware {
+class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware{
+    //Shizuku.OnBinderReceivedListener, Shizuku.OnBinderDeadListener, Shizuku.OnRequestPermissionResultListener {
+
+
+
+
+    private lateinit var mService: EcosedKitPlugin
+    private lateinit var poem: ArrayList<String>
+
+    override fun onCreate() {
+        super.onCreate()
+        mService = this@EcosedKitPlugin
+
+        poem = arrayListOf()
+        poem.add("不向焦虑与抑郁投降，这个世界终会有我们存在的地方。")
+        poem.add("把喜欢的一切留在身边，这便是努力的意义。")
+        poem.add("治愈、温暖，这就是我们最终幸福的结局。")
+        poem.add("我有一个梦，也许有一天，灿烂的阳光能照进黑暗森林。")
+        poem.add("如果必须要失去，那么不如一开始就不曾拥有。")
+        poem.add("我们的终点就是与幸福同在。")
+        poem.add("孤独的人不会伤害别人，只会不断地伤害自己罢了。")
+        poem.add("如果你能记住我的名字，如果你们都能记住我的名字，也许我或者我们，终有一天能自由地生存着。")
+        poem.add("对于所有生命来说，不会死亡的绝望，是最可怕的审判。")
+        poem.add("我不曾活着，又何必害怕死亡。")
+
+
+        val notification = buildNotification()
+        //startForeground(notificationId, notification)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+
+    override fun onBind(intent: Intent): IBinder {
+        return object : EcosedKit.Stub() {
+            override fun getFrameworkVersion(): String = frameworkVersion()
+            override fun getShizukuVersion(): String = shizukuVersion()
+            override fun getChineseCale(): String = chineseCale()
+            override fun getOnePoem(): String = onePoem()
+            override fun isWatch(): Boolean = watch()
+            override fun isUseDynamicColors(): Boolean = dynamicColors()
+            override fun openDesktopSettings() = taskbarSettings()
+            override fun openEcosedSettings() = ecosedSettings()
+        }
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+//    override fun onBinderReceived() {
+//
+//    }
+//
+//    override fun onBinderDead() {
+//
+//    }
+//
+//    override fun onRequestPermissionResult(requestCode: Int, grantResult: Int) {
+//
+//    }
+
+    private val mUserServiceArgs = Shizuku.UserServiceArgs(
+        ComponentName(AppUtils.getAppPackageName(), UserService().javaClass.name)
+    )
+        .daemon(false)
+        .processNameSuffix("service")
+        .debuggable(BuildConfig.DEBUG)
+        .version(AppUtils.getAppVersionCode())
+
+    private fun check() {
+        try {
+            if (Shizuku.checkSelfPermission() != PackageManager.PERMISSION_GRANTED) {
+                Shizuku.requestPermission(0)
+            }
+        } catch (e: Exception) {
+            if (e.javaClass == IllegalStateException().javaClass) {
+                //PopTip.show("Shizuku未激活")
+            }
+        }
+    }
+
+    private fun frameworkVersion(): String {
+        return AppUtils.getAppVersionName()
+    }
+
+    private fun shizukuVersion(): String {
+        return try {
+            "Shizuku ${Shizuku.getVersion()}"
+        } catch (e: Exception) {
+            Log.getStackTraceString(e)
+        }
+    }
+
+    private fun chineseCale(): String {
+        return ""
+    }
+
+    private fun onePoem(): String {
+        return poem[(poem.indices).random()]
+    }
+
+    private fun watch(): Boolean {
+        return getSystemService(
+            UiModeManager::class.java
+        ).currentModeType == Configuration.UI_MODE_TYPE_WATCH
+    }
+
+    private fun dynamicColors(): Boolean {
+        return true
+    }
+
+    private fun taskbarSettings() {
+        CoroutineScope(
+            context = Dispatchers.Main
+        ).launch {
+
+        }
+    }
+    private fun ecosedSettings() {
+        CoroutineScope(
+            context = Dispatchers.Main
+        ).launch {
+
+        }
+    }
+
+    private fun buildNotification(): Notification {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            PermissionUtils.permission(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        val notification = NotificationCompat.Builder(
+            this@EcosedKitPlugin,
+            ""
+        )
+            .setContentTitle(AppUtils.getAppName())
+            .setContentText("服务正在运行")
+        //    .setSmallIcon(R.drawable.baseline_keyboard_command_key_24)
+            .build()
+
+        notification.flags = Notification.FLAG_ONGOING_EVENT
+
+        return notification
+    }
+
+
+
 
     private lateinit var mMethodChannel: MethodChannel
-
-    // 附加基本上下文
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-    }
 
     // 插件附加到引擎
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -76,7 +248,6 @@ class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.Metho
     override fun onAttachedToActivity(
         binding: ActivityPluginBinding,
     ) = engineUnit {
-        attachBaseContext(base = binding.activity.baseContext)
         getActivity(activity = binding.activity)
         getLifecycle(lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding))
         attach()
@@ -327,7 +498,7 @@ class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.Metho
          * 设置方法调用.
          * @param handler 执行方法时调用EcosedMethodCallHandler.
          */
-        internal fun setMethodCallHandler(handler: EcosedPlugin) {
+        fun setMethodCallHandler(handler: EcosedPlugin) {
             mPlugin = handler
         }
 
@@ -335,7 +506,7 @@ class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.Metho
          * 获取上下文.
          * @return Context.
          */
-        internal fun getContext(): Context {
+        fun getContext(): Context {
             return mBinding.getContext()
         }
 
@@ -343,7 +514,7 @@ class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.Metho
          * 是否调试模式.
          * @return Boolean.
          */
-        internal fun isDebug(): Boolean {
+        fun isDebug(): Boolean {
             return mBinding.isDebug()
         }
 
@@ -577,7 +748,7 @@ class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.Metho
          * @param bundle 通过Bundle传递参数.
          * @return 返回方法执行后的返回值,类型为Any?.
          */
-        internal fun <T> execMethodCall(
+        fun <T> execMethodCall(
             channel: String,
             method: String,
             bundle: Bundle?,
@@ -616,6 +787,231 @@ class EcosedKitPlugin : ContextWrapper(null), FlutterPlugin, MethodChannel.Metho
 
             private const val channelName: String = "termplux_engine"
 
+        }
+    }
+
+
+    internal interface EcosedCallBack {
+
+        /** 在服务绑定成功时回调 */
+        fun onEcosedConnected()
+
+        /** 在服务解绑或意外断开链接时回调 */
+        fun onEcosedDisconnected()
+
+        /** 在服务端服务未启动时绑定服务时回调 */
+        fun onEcosedDead()
+
+        /** 在未绑定服务状态下调用API时回调 */
+        fun onEcosedUnbind()
+    }
+
+    private class TermPluxClient private constructor() : EcosedPlugin(), ServiceConnection, EcosedCallBack {
+
+        override val channel: String
+            get() = mChannelName
+
+        private lateinit var mIntent: Intent
+
+        /** 服务AIDL接口 */
+        private var mFramework: EcosedKit? = null
+
+        /** 服务绑定状态 */
+        private var mIsBind: Boolean = false
+
+        override fun onEcosedAdded(binding: PluginBinding) {
+            super.onEcosedAdded(binding)
+            mIntent = Intent(this@TermPluxClient, EcosedKitPlugin().javaClass)
+            mIntent.action = action
+
+            startService(mIntent)
+            bindEcosed()
+
+            Toast.makeText(this, "client", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onEcosedMethodCall(call: EcosedMethodCall, result: EcosedResult) {
+            super.onEcosedMethodCall(call, result)
+            when (call.method) {
+                mMethodDebug -> result.success(isDebug)
+                mMethodIsBinding -> result.success(mIsBind)
+                mMethodStartService -> startService(mIntent)
+                mMethodBindService -> bindEcosed()
+                mMethodUnbindService -> unbindEcosed()
+
+                "" -> result.success({
+
+                })
+
+                mMethodShizukuVersion -> result.success(getShizukuVersion())
+                else -> result.notImplemented()
+            }
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            mFramework = EcosedKit.Stub.asInterface(service)
+            when {
+                mFramework != null -> {
+                    mIsBind = true
+                    callBack {
+                        onEcosedConnected()
+                    }
+                }
+
+                else -> if (isDebug) Log.e(
+                    tag, "AIDL接口获取失败 - onServiceConnected"
+                )
+            }
+            when {
+                isDebug -> Log.i(
+                    tag, "服务已连接 - onServiceConnected"
+                )
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mIsBind = false
+            mFramework = null
+            unbindService(this)
+            callBack {
+                onEcosedDisconnected()
+            }
+            if (isDebug) {
+                Log.i(tag, "服务意外断开连接 - onServiceDisconnected")
+            }
+        }
+
+        override fun onBindingDied(name: ComponentName?) {
+            super.onBindingDied(name)
+        }
+
+        override fun onNullBinding(name: ComponentName?) {
+            super.onNullBinding(name)
+            if (isDebug) {
+                Log.e(tag, "Binder为空 - onNullBinding")
+            }
+        }
+
+        override fun onEcosedConnected() {
+            Toast.makeText(this@TermPluxClient, "onEcosedConnected", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onEcosedDisconnected() {
+            Toast.makeText(this@TermPluxClient, "onEcosedDisconnected", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onEcosedDead() {
+            Toast.makeText(this@TermPluxClient, "onEcosedDead", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onEcosedUnbind() {
+            Toast.makeText(this@TermPluxClient, "onEcosedUnbind", Toast.LENGTH_SHORT).show()
+        }
+
+        /**
+         * 绑定服务
+         */
+        private fun bindEcosed() {
+            try {
+                if (!mIsBind) {
+                    bindService(
+                        mIntent,
+                        this@TermPluxClient,
+                        Context.BIND_AUTO_CREATE
+                    ).let {
+                        if (!it) callBack {
+                            onEcosedDead()
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                if (isDebug) {
+                    Log.e(tag, "bindEcosed", e)
+                }
+            }
+        }
+
+        /**
+         * 解绑服务
+         */
+        private fun unbindEcosed() {
+            try {
+                if (mIsBind) {
+                    unbindService(
+                        this@TermPluxClient
+                    ).run {
+                        mIsBind = false
+                        mFramework = null
+                        callBack {
+                            onEcosedDisconnected()
+                        }
+                        if (isDebug) {
+                            Log.i(tag, "服务已断开连接 - onServiceDisconnected")
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                if (isDebug) {
+                    Log.e(tag, "unbindEcosed", e)
+                }
+            }
+        }
+
+        private fun getShizukuVersion(): String? {
+            return try {
+                if (mIsBind) {
+                    if (mFramework != null) {
+                        mFramework!!.shizukuVersion
+                    } else {
+                        callBack {
+                            onEcosedUnbind()
+                        }
+                        null
+                    }
+                } else {
+                    callBack {
+                        onEcosedUnbind()
+                    }
+                    null
+                }
+            } catch (e: Exception) {
+                if (isDebug) {
+                    Log.e(tag, "getShizukuVersion", e)
+                }
+                null
+            }
+        }
+
+        private fun callBack(
+            function: EcosedCallBack.() -> Unit,
+        ) {
+            this@TermPluxClient.function()
+        }
+
+        companion object {
+            internal const val mChannelName: String = "ecosed_droid"
+            internal const val mMethodDebug: String = "is_binding"
+            internal const val mMethodIsBinding: String = "is_debug"
+            internal const val mMethodStartService: String = "start_service"
+            internal const val mMethodBindService: String = "bind_service"
+            internal const val mMethodUnbindService: String = "unbind_service"
+
+            internal const val mMethodShizukuVersion: String = "shizuku_version"
+
+            internal const val tag: String = "EcosedClient"
+            internal const val action: String = "io.ecosed.droid.action"
+            fun build(): TermPluxClient = TermPluxClient()
+        }
+    }
+
+    private class UserService : IUserService.Stub() {
+
+        override fun destroy() {
+            exitProcess(0)
+        }
+
+        override fun exit() {
+            exitProcess(0)
         }
     }
 
