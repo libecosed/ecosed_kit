a8e727882982e88cfc709cb65ed8e954021c7234
王泳淇 <815099246@qq.com>
2024-01-07 14:49:01 +0800

    commit

--- android/src/main/kotlin/io/ecosed/kit/EcosedKitPlugin.kt
+++ android/src/main/kotlin/io/ecosed/kit/EcosedKitPlugin.kt
@@ -42,6 +42,32 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
     private lateinit var poem: ArrayList<String>
 
 
+    private lateinit var mActivity: Activity
+    private lateinit var mLifecycle: Lifecycle
+
+    /** 插件绑定器. */
+    private var mBinding: PluginBinding? = null
+
+    /** 插件列表. */
+    private var mPluginList: ArrayList<EcosedPlugin>? = null
+
+    private val mBaseDebug: Boolean = AppUtils.isAppDebug()
+
+
+    private var execResult: Any? = null
+
+    private var mFullDebug: Boolean = false
+
+
+    private lateinit var mEcosedServicesIntent: Intent
+
+    /** 服务AIDL接口 */
+    private var mAIDL: EcosedKit? = null
+
+    /** 服务绑定状态 */
+    private var mIsBind: Boolean = false
+
+
     override fun onCreate() {
         super<Service>.onCreate()
 
@@ -245,17 +271,53 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
     }
 
     private interface EngineWrapper : FlutterPluginProxy {
-
         fun <T> execMethodCall(channel: String, method: String, bundle: Bundle?): T?
     }
 
-    private interface ServiceWrapper {
-        fun getBinder(intent: Intent): IBinder
+    /**
+     * 用于调用方法的接口.
+     */
+    private interface EcosedMethodCall {
+
+        /**
+         * 要调用的方法名.
+         */
+        val method: String?
+
+        /**
+         * 要传入的参数.
+         */
+        val bundle: Bundle?
     }
 
+    /**
+     * 方法调用结果回调.
+     */
+    private interface EcosedResult {
 
+        /**
+         * 处理成功结果.
+         * @param result 处理成功结果,注意可能为空.
+         */
+        fun success(result: Any?)
 
+        /**
+         * 处理错误结果.
+         * @param errorCode 错误代码.
+         * @param errorMessage 错误消息,注意可能为空.
+         * @param errorDetails 详细信息,注意可能为空.
+         */
+        fun error(
+            errorCode: String,
+            errorMessage: String?,
+            errorDetails: Any?
+        ): Nothing
 
+        /**
+         * 处理对未实现方法的调用.
+         */
+        fun notImplemented()
+    }
 
     private interface EcosedCallBack {
 
@@ -272,11 +334,238 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
         fun onEcosedUnbind()
     }
 
+    private interface ServiceWrapper {
+        fun getBinder(intent: Intent): IBinder
+    }
+
+    /**
+     * 作者: wyq0918dev
+     * 仓库: https://github.com/ecosed/EcosedDroid
+     * 时间: 2023/10/01
+     * 描述: 基本插件
+     * 文档: https://github.com/ecosed/EcosedDroid/blob/master/README.md
+     */
+    private abstract class EcosedPlugin : ContextWrapper(null) {
+
+        /** 插件通道 */
+        private lateinit var mPluginChannel: PluginChannel
+
+        /** 是否调试模式 */
+        private var mDebug: Boolean = false
+
+        /**
+         * 附加基本上下文
+         */
+        override fun attachBaseContext(base: Context?) {
+            super.attachBaseContext(base)
+        }
+
+        /**
+         * 插件添加时执行
+         */
+        open fun onEcosedAdded(binding: PluginBinding) {
+            // 初始化插件通道
+            mPluginChannel = PluginChannel(
+                binding = binding, channel = channel
+            )
+            // 插件附加基本上下文
+            attachBaseContext(
+                base = mPluginChannel.getContext()
+            )
+            // 获取是否调试模式
+            mDebug = mPluginChannel.isDebug()
+            // 设置调用
+            mPluginChannel.setMethodCallHandler(
+                handler = this@EcosedPlugin
+            )
+        }
+
+        /** 获取插件通道 */
+        val getPluginChannel: PluginChannel
+            get() = mPluginChannel
+
+        /** 需要子类重写的通道名称 */
+        abstract val channel: String
+
+        /** 供子类使用的判断调试模式的接口 */
+        protected val isDebug: Boolean = mDebug
+
+        /**
+         * 插件调用方法
+         */
+        open fun onEcosedMethodCall(call: EcosedMethodCall, result: EcosedResult) = Unit
+    }
+
+    /**
+     * 作者: wyq0918dev
+     * 仓库: https://github.com/ecosed/plugin
+     * 时间: 2023/09/02
+     * 描述: 插件绑定器
+     * 文档: https://github.com/ecosed/plugin/blob/master/README.md
+     */
+    private class PluginBinding(
+        context: Context,
+        debug: Boolean,
+    ) {
+
+        /** 应用程序全局上下文. */
+        private val mContext: Context = context
+
+        /** 是否调试模式. */
+        private val mDebug: Boolean = debug
+
+        /**
+         * 获取上下文.
+         * @return Context.
+         */
+        fun getContext(): Context {
+            return mContext
+        }
+
+        /**
+         * 是否调试模式.
+         * @return Boolean.
+         */
+        fun isDebug(): Boolean {
+            return mDebug
+        }
+    }
+
+    /**
+     * 作者: wyq0918dev
+     * 仓库: https://github.com/ecosed/plugin
+     * 时间: 2023/09/02
+     * 描述: 插件通信通道
+     * 文档: https://github.com/ecosed/plugin/blob/master/README.md
+     */
+    private class PluginChannel(binding: PluginBinding, channel: String) {
+
+        /** 插件绑定器. */
+        private var mBinding: PluginBinding = binding
+
+        /** 插件通道. */
+        private var mChannel: String = channel
+
+        /** 方法调用处理接口. */
+        private var mPlugin: EcosedPlugin? = null
+
+        /** 方法名. */
+        private var mMethod: String? = null
+
+        /** 参数Bundle. */
+        private var mBundle: Bundle? = null
+
+        /** 返回结果. */
+        private var mResult: Any? = null
+
+        /**
+         * 设置方法调用.
+         * @param handler 执行方法时调用EcosedMethodCallHandler.
+         */
+        fun setMethodCallHandler(handler: EcosedPlugin) {
+            mPlugin = handler
+        }
+
+        /**
+         * 获取上下文.
+         * @return Context.
+         */
+        fun getContext(): Context {
+            return mBinding.getContext()
+        }
+
+        /**
+         * 是否调试模式.
+         * @return Boolean.
+         */
+        fun isDebug(): Boolean {
+            return mBinding.isDebug()
+        }
+
+        /**
+         * 获取通道.
+         * @return 通道名称.
+         */
+        fun getChannel(): String {
+            return mChannel
+        }
+
+        /**
+         * 执行方法回调.
+         * @param name 通道名称.
+         * @param method 方法名称.
+         * @return 方法执行后的返回值.
+         */
+        @Suppress("UNCHECKED_CAST")
+        fun <T> execMethodCall(name: String, method: String?, bundle: Bundle?): T? {
+            mMethod = method
+            mBundle = bundle
+            if (name == mChannel) {
+                mPlugin?.onEcosedMethodCall(
+                    call = call,
+                    result = result
+                )
+            }
+            return mResult as T?
+        }
+
+        /** 用于调用方法的接口. */
+        private val call: EcosedMethodCall = object : EcosedMethodCall {
+
+            /**
+             * 要调用的方法名.
+             */
+            override val method: String?
+                get() = mMethod
+
+            /**
+             * 要传入的参数.
+             */
+            override val bundle: Bundle?
+                get() = mBundle
+        }
+
+        /** 方法调用结果回调. */
+        private val result: EcosedResult = object : EcosedResult {
+
+            /**
+             * 处理成功结果.
+             */
+            override fun success(result: Any?) {
+                mResult = result
+            }
+
+            /**
+             * 处理错误结果.
+             */
+            override fun error(
+                errorCode: String,
+                errorMessage: String?,
+                errorDetails: Any?
+            ): Nothing = error(
+                message = "错误代码:$errorCode\n" +
+                        "错误消息:$errorMessage\n" +
+                        "详细信息:$errorDetails"
+            )
+
+            /**
+             * 处理对未实现方法的调用.
+             */
+            override fun notImplemented() {
+                mResult = null
+            }
+        }
+
+    }
+
 
     /**
      ***********************************************************************************************
      */
     private val mFramework = object : EcosedPlugin(), FlutterPluginProxy {
+
+        override val channel: String
+            get() = frameworkChannelName
 
         override fun onEcosedAdded(binding: PluginBinding) {
             super.onEcosedAdded(binding)
@@ -301,9 +590,6 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
         override fun attach() = engineUnit {
             attach()
         }
-
-        override val channel: String
-            get() = frameworkChannelName
     }
 
     private val mEngine = object : EcosedPlugin(), EngineWrapper {
@@ -518,12 +804,29 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
         override fun onRequestPermissionResult(requestCode: Int, grantResult: Int) {
 
         }
-
-
     }
 
+    private val mUserServiceArgs = Shizuku.UserServiceArgs(
+        ComponentName(AppUtils.getAppPackageName(), UserService().javaClass.name)
+    )
+        .daemon(false)
+        .processNameSuffix("service")
+        .debuggable(BuildConfig.DEBUG)
+        .version(AppUtils.getAppVersionCode())
 
+    private class UserService : IUserService.Stub() {
+
+        override fun destroy() {
+            exitProcess(0)
+        }
+
+        override fun exit() {
+            exitProcess(0)
+        }
+    }
     /**
+     ***********************************************************************************************
+     * Unit
      ***********************************************************************************************
      */
 
@@ -537,19 +840,17 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
 
     private fun <R> pluginUnit(
         content: ArrayList<EcosedPlugin>.(PluginBinding) -> R
-    ): R {
-        return content.invoke(
-            arrayListOf(
-                mFramework,
-                mEngine,
-                mClient
-            ),
-            PluginBinding(
-                context = mActivity,
-                debug = mBaseDebug
-            )
+    ): R = content.invoke(
+        arrayListOf(
+            mFramework,
+            mEngine,
+            mClient
+        ),
+        PluginBinding(
+            context = mActivity,
+            debug = mBaseDebug
         )
-    }
+    )
 
     /**
      ***********************************************************************************************
@@ -563,13 +864,7 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
 
 
 
-    private val mUserServiceArgs = Shizuku.UserServiceArgs(
-        ComponentName(AppUtils.getAppPackageName(), UserService().javaClass.name)
-    )
-        .daemon(false)
-        .processNameSuffix("service")
-        .debuggable(BuildConfig.DEBUG)
-        .version(AppUtils.getAppVersionCode())
+
 
     private fun check() {
         try {
@@ -657,305 +952,11 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
 
 
 
-    /**
-     * 用于调用方法的接口.
-     */
-    private interface EcosedMethodCall {
-
-        /**
-         * 要调用的方法名.
-         */
-        val method: String?
-
-        /**
-         * 要传入的参数.
-         */
-        val bundle: Bundle?
-    }
-
-
-    /**
-     * 作者: wyq0918dev
-     * 仓库: https://github.com/ecosed/EcosedDroid
-     * 时间: 2023/10/01
-     * 描述: 基本插件
-     * 文档: https://github.com/ecosed/EcosedDroid/blob/master/README.md
-     */
-    private abstract class EcosedPlugin : ContextWrapper(null) {
-
-        /** 插件通道 */
-        private lateinit var mPluginChannel: PluginChannel
-
-        /** 是否调试模式 */
-        private var mDebug: Boolean = false
-
-        /**
-         * 附加基本上下文
-         */
-        override fun attachBaseContext(base: Context?) {
-            super.attachBaseContext(base)
-        }
-
-        /**
-         * 插件添加时执行
-         */
-        open fun onEcosedAdded(binding: PluginBinding) {
-            // 初始化插件通道
-            mPluginChannel = PluginChannel(
-                binding = binding, channel = channel
-            )
-            // 插件附加基本上下文
-            attachBaseContext(
-                base = mPluginChannel.getContext()
-            )
-            // 获取是否调试模式
-            mDebug = mPluginChannel.isDebug()
-            // 设置调用
-            mPluginChannel.setMethodCallHandler(
-                handler = this@EcosedPlugin
-            )
-        }
-
-        /** 获取插件通道 */
-        val getPluginChannel: PluginChannel
-            get() = mPluginChannel
-
-        /** 需要子类重写的通道名称 */
-        abstract val channel: String
-
-        /** 供子类使用的判断调试模式的接口 */
-        protected val isDebug: Boolean = mDebug
-
-        /**
-         * 插件调用方法
-         */
-        open fun onEcosedMethodCall(call: EcosedMethodCall, result: EcosedResult) = Unit
-    }
-
-
-    /**
-     * 方法调用结果回调.
-     */
-    private interface EcosedResult {
-
-        /**
-         * 处理成功结果.
-         * @param result 处理成功结果,注意可能为空.
-         */
-        fun success(result: Any?)
-
-        /**
-         * 处理错误结果.
-         * @param errorCode 错误代码.
-         * @param errorMessage 错误消息,注意可能为空.
-         * @param errorDetails 详细信息,注意可能为空.
-         */
-        fun error(
-            errorCode: String,
-            errorMessage: String?,
-            errorDetails: Any?
-        ): Nothing
-
-        /**
-         * 处理对未实现方法的调用.
-         */
-        fun notImplemented()
-    }
-
-    /**
-     * 作者: wyq0918dev
-     * 仓库: https://github.com/ecosed/plugin
-     * 时间: 2023/09/02
-     * 描述: 插件绑定器
-     * 文档: https://github.com/ecosed/plugin/blob/master/README.md
-     */
-    private class PluginBinding(
-        context: Context,
-        debug: Boolean,
-    ) {
-
-        /** 应用程序全局上下文. */
-        private val mContext: Context = context
-
-        /** 是否调试模式. */
-        private val mDebug: Boolean = debug
-
-        /**
-         * 获取上下文.
-         * @return Context.
-         */
-        fun getContext(): Context {
-            return mContext
-        }
-
-        /**
-         * 是否调试模式.
-         * @return Boolean.
-         */
-        fun isDebug(): Boolean {
-            return mDebug
-        }
-    }
-
-    /**
-     * 作者: wyq0918dev
-     * 仓库: https://github.com/ecosed/plugin
-     * 时间: 2023/09/02
-     * 描述: 插件通信通道
-     * 文档: https://github.com/ecosed/plugin/blob/master/README.md
-     */
-    private class PluginChannel(binding: PluginBinding, channel: String) {
-
-        /** 插件绑定器. */
-        private var mBinding: PluginBinding = binding
-
-        /** 插件通道. */
-        private var mChannel: String = channel
-
-        /** 方法调用处理接口. */
-        private var mPlugin: EcosedPlugin? = null
-
-        /** 方法名. */
-        private var mMethod: String? = null
-
-        /** 参数Bundle. */
-        private var mBundle: Bundle? = null
-
-        /** 返回结果. */
-        private var mResult: Any? = null
-
-        /**
-         * 设置方法调用.
-         * @param handler 执行方法时调用EcosedMethodCallHandler.
-         */
-        fun setMethodCallHandler(handler: EcosedPlugin) {
-            mPlugin = handler
-        }
-
-        /**
-         * 获取上下文.
-         * @return Context.
-         */
-        fun getContext(): Context {
-            return mBinding.getContext()
-        }
-
-        /**
-         * 是否调试模式.
-         * @return Boolean.
-         */
-        fun isDebug(): Boolean {
-            return mBinding.isDebug()
-        }
-
-        /**
-         * 获取通道.
-         * @return 通道名称.
-         */
-        fun getChannel(): String {
-            return mChannel
-        }
-
-        /**
-         * 执行方法回调.
-         * @param name 通道名称.
-         * @param method 方法名称.
-         * @return 方法执行后的返回值.
-         */
-        @Suppress("UNCHECKED_CAST")
-        fun <T> execMethodCall(name: String, method: String?, bundle: Bundle?): T? {
-            mMethod = method
-            mBundle = bundle
-            if (name == mChannel) {
-                mPlugin?.onEcosedMethodCall(
-                    call = call,
-                    result = result
-                )
-            }
-            return mResult as T?
-        }
-
-        /** 用于调用方法的接口. */
-        private val call: EcosedMethodCall = object : EcosedMethodCall {
-
-            /**
-             * 要调用的方法名.
-             */
-            override val method: String?
-                get() = mMethod
-
-            /**
-             * 要传入的参数.
-             */
-            override val bundle: Bundle?
-                get() = mBundle
-        }
-
-        /** 方法调用结果回调. */
-        private val result: EcosedResult = object : EcosedResult {
-
-            /**
-             * 处理成功结果.
-             */
-            override fun success(result: Any?) {
-                mResult = result
-            }
-
-            /**
-             * 处理错误结果.
-             */
-            override fun error(
-                errorCode: String,
-                errorMessage: String?,
-                errorDetails: Any?
-            ): Nothing = error(
-                message = "错误代码:$errorCode\n" +
-                        "错误消息:$errorMessage\n" +
-                        "详细信息:$errorDetails"
-            )
-
-            /**
-             * 处理对未实现方法的调用.
-             */
-            override fun notImplemented() {
-                mResult = null
-            }
-        }
-
-    }
-
-    private lateinit var mActivity: Activity
-    private lateinit var mLifecycle: Lifecycle
-
-    /** 插件绑定器. */
-    private var mBinding: PluginBinding? = null
-
-    /** 插件列表. */
-    private var mPluginList: ArrayList<EcosedPlugin>? = null
-
-    private val mBaseDebug: Boolean = AppUtils.isAppDebug()
-
-
-    private var execResult: Any? = null
-
-    private var mFullDebug: Boolean = false
 
 
 
 
 
-
-
-
-
-
-    private lateinit var mEcosedServicesIntent: Intent
-
-    /** 服务AIDL接口 */
-    private var mAIDL: EcosedKit? = null
-
-    /** 服务绑定状态 */
-    private var mIsBind: Boolean = false
 
 
 
@@ -1041,16 +1042,7 @@ class EcosedKitPlugin : Service(), FlutterPlugin, MethodChannel.MethodCallHandle
         mClient.function()
     }
 
-    private class UserService : IUserService.Stub() {
 
-        override fun destroy() {
-            exitProcess(0)
-        }
-
-        override fun exit() {
-            exitProcess(0)
-        }
-    }
 
     private companion object {
 
